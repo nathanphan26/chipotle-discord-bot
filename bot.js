46db7729d71be782cf8c3e7a5a758d495ea014a8
Nathan Phan <nathphan510@gmail.com>
2022-10-20 12:35:50 -0700

    Added Text Message Link

--- bot.js
+++ bot.js
@@ -15,9 +15,17 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+const prefix = ">";
 // This code will run once the bot receives any message.
 client.on('messageCreate', message => {
-    if (message.content === 'ping') message.reply('pong')
+    // Exit and stop if it's not there
+    if (!message.content.startsWith(prefix)) return;
+
+    // Get Chipotle Code
+    const code = message.content.substring(1)
+    // Format Reply Message
+    const reply = `<sms://888222/;?&body=${code}>`
+    message.reply(reply)
 })
 
 client.login(process.env.DISCORD_TOKEN);
